5a66d8b7095a8a5b38aec2a5525d8c74ebcdb171
Francois Michonneau <francois.michonneau@gmail.com>
2014-09-16 11:34:12 -0400

    initial commit of tree_of_life interface

--- R/tol.R
+++ R/tol.R
@@ -15,10 +15,84 @@ tol_about <- function() {
 ##'
 ##' Most recent common ancestor
 ##' @title get MRCA
-##' @param nds
+##' @param ott_ids
+##' @param node_ids
 ##' @return the MRCA
 ##' @author Francois Michonneau
+##' @examples
+##' test1 <- tol_mrca(ott_ids=list("ott_ids" = c(412129, 536234)))
+##' test2 <- tol_mrca(ott_ids=list("ott_ids" = c(415255)), node_ids=c(341556))
 ##' @export
-tol_mrca <- function(q=list("ott_ids"=c(412129, 536234))) {
+tol_mrca <- function(ott_ids, node_ids) {
+    if (missing(node_ids) && !missing(ott_ids)) q <- ott_ids
+    if (!missing(node_ids) && missing(ott_ids)) q <- node_ids
+    if (!missing(node_ids) && !missing(ott_ids)) q <- c(ott_ids, node_ids)
     otl_POST(path="tree_of_life/mrca", body=q)
+}
+
+##' Extract subtree from a node or an OTT
+##'
+##' Return a complete subtree of the draft tree descended from some
+##' specified node. The node to use as the start node may be specified
+##' using either a node id or an ott id, but not both. If the
+##' specified node is not in the synthetic tree (or is entirely absent
+##' from the graph), an error will be returned.
+##' @title Extract subtree
+##' @return something
+##' @author Francois Michonneau
+##' @param node_id The node id of the node in the tree that should
+##' serve as the root of the tree returned. This argument may not be
+##' used in combination with ott_id.
+##' @param ott_id The ott id of the node in the tree that should serve
+##' as the root of the tree returned. This argument may not be used in
+##' combination with node_id.
+##' @param tree_id The identifier for the synthesis tree. We currently
+##' only support a single draft tree in the db at a time, so this
+##' argument is superfluous and may be safely ignored.
+##' @export
+tol_subtree <- function(node_id, ott_id, tree_id) {
+    if (!missing(node_id) && !missing(ott_id)) {
+        stop("Use only node_id OR ott_id")
+    }
+    if (!missing(tree_id)) {
+        stop("tree_id is currently ignored")
+    }
+    if (missing(node_id) && !missing(ott_id)) {
+        q <- ott_id
+    }
+    if (!missing(node_id) && missing(ott_id)) {
+        q <- node_id
+    }
+    otl_POST(path="tree_of_life/subtree", body=q)
+}
+
+##' Extract induced subtree
+##'
+##' Return a tree with tips corresponding to the nodes identified in
+##' the input set(s), that is consistent with topology of the current
+##' draft tree. This tree is equivalent to the minimal subtree induced
+##' on the draft tree by the set of identified nodes. Any combination
+##' of node ids and ott ids may be used as input. Nodes or ott ids
+##' that do not correspond to any found nodes in the graph, or which
+##' are in the graph but are absent from the synthetic tree, will be
+##' identified in the output (but will of course not be present in the
+##' resulting induced tree). Branch lengths in the result may be
+##' arbitrary, and the leaf labels of the tree may either be taxonomic
+##' names or (for nodes not corresponding directly to named taxa) node
+##' ids.
+##' @title induced subtree
+##' @param node_ids Node ids indicating nodes to be used as tips in
+##' the induced tree
+##' @param ott_ids OTT ids indicating nodes to be used as tips in the
+##' induced tree
+##' @return something
+##' @author Francois Michonneau
+##' @examples
+##' {"ott_ids":[292466, 501678, 267845, 666104, 316878, 102710, 176458]}
+##' @export
+tol_induced_subtree <- function(node_ids, ott_ids) {
+    if (missing(node_ids) && !missing(ott_ids)) q <- ott_ids
+    if (!missing(node_ids) && missing(ott_ids)) q <- node_ids
+    if (!missing(node_ids) && !missing(ott_ids)) q <- c(ott_ids, node_ids)
+    otl_POST("tree_of_life/induced_subtree", body=q)
 }
