eee86ef79d6c9e5ad0ad9b11f1c7634af179a846
David Winter <david.winter@gmail.com>
2014-09-18 12:52:59 -0400

    function to test whole json file of tests

--- tests/test-all.R
+++ tests/test-all.R
@@ -79,16 +79,17 @@ test_contains_error <- function(response, test_block){
     sapply(errs, function(e) expect_that(reponse, contains_error(e)))
 }
 
+##convience functions 
 
-
-make_request <- function(json_test){
-    do.call(what=json_test$test_function, args=json_test$test_input)
+type_map <- function(json_type){
+    switch(json_type, 
+           "dict" = "list",
+           stop(sprintf("unknown json type in testing file: %s", json_type))
+          )
 }
 
 
-
-
-expect_map <- function(test_type){
+test_map <- function(test_type){
     switch(test_type,
            "contains"    = test_contains,
            "equals"      = test_equals
@@ -99,79 +100,34 @@ expect_map <- function(test_type){
            stop(sprintf("Unkown error type in JSON test: %s", test_type))
            )
 }
+
+make_request <- function(json_test){
+    do.call(what=json_test$test_function, args=json_test$test_input)
 }
-
-#functionals that start with a complete test block
-
 
 
 test_that_json_test <- function(test_obj, test_name){
-    tests_to_run <- names(test_obj[test_name][tests])
-    
+    tests_to_run <- names(test_obj$test_name$tests)
     if(identical(tests_to_run, "error")){
-        test_that(test_name, expect_error(make_request(test_obj[test_name])))
+        expect_error(make_request(test_obj[test_name]))
     }
-    else{
 
-        test_that(test_name,
+    else{
         response <- make_request(test_obj[test_name])
         for(i in 1:length(tests_to_run)){
-            expect_fxn <- expect_map(tests_to_run[i])
-
-
-           
-
-
+            test_block <- test_obj$test_name$tests[[ tests_to_run[i] ]]
+            test_fxn <- test_map(tests_to_run[i]) 
+            test_fxn(test_block)
         }
     }
 }
 
-
-
-
-
-
-##Map JSON object types to R-equivalent
-test_of_type <- function(response, test_obj){
-    rtype <- test_obj[
-    expect_is(response, type_map[test_type]
-
-
-
-
-
-type_map <- function(json_type){
-    switch(json_type, 
-           "dict" = "list",
-           stop(sprintf("unknown json type in testing file: %s", json_type))
+test_file <- function(json_obj){
+   all_tests <- names(json_obj)
+   for(i in 1:length(all_tests)){
+       test_that(all_tests[i],
+          test_that_json_test(json_obj, all_tests[i])
           )
-}
-
-
-run_expectations <- function(test_type, response, test_obj){
-    test_block <- test_obj[test_type]
-    switch(test_type, 
-     "of_type" = expect_is(response, type_map(test_block[1])),
-     "equals"  = expect_equal(response[ test_block[1] ], is_equal(test_block[2])),
-     "contains" = apply(test_block, 1, function(r) expectation( r[[1]] %in% names(response),
-                                                                fail_msg=r[[2]]) ),
-     stop(sprintf("unkown test-type in json file: ", test_type))
-     )
-}
-
-
-build_test <- function(json_test, desc){
-    tests_to_run <- names(json_test$tests)
-    if(identical("error", tests_to_run)){
-        test_that(desc,
-            expect_error(do.call(json_test$test_function, 
-                                 args=json_test$test_input)))
-    }
-    else{
-        response <- do.call(json_test$test_function, args=json_test$test_input)
-        test_that(desc, 
-            sapply(tests_to_run, run_expectations, response, json_test$tests)
-        )
-    }
+   }
 }
 
