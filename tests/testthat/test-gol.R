754ed81683e6e200b95a520a65676c5d1a822e8c
Francois Michonneau <francois.michonneau@gmail.com>
2015-06-03 19:00:46 -0400

    tests for gol

--- tests/testthat/test-gol.R
+++ tests/testthat/test-gol.R
@@ -5,3 +5,10 @@ test_that("gol about", {
               expect_true(inherits(tmp_gol, "gol"))
               expect_equal(length(tmp_gol),  6L)
           })
+
+test_that("gol node info. If any of these tests break, it means that the doc for this function needs to be updated.", {
+              expect_true(all(names(gol_node_info) %in%
+                                c("in_graph", "tree_id", "name","rank","ott_id", "num_tips",
+                                  "tree_sources", "tax_source", "synth_sources",
+                                  "node_id", "in_synth_tree", "num_synth_children")))
+          })
