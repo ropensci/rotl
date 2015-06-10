a59314af607419ecf43ffd1a707ee6f54b1b263e
Francois Michonneau <francois.michonneau@gmail.com>
2015-06-10 17:16:11 -0400

    add test to make sure doc is accurate

--- tests/testthat/test-gol.R
+++ tests/testthat/test-gol.R
@@ -1,10 +1,16 @@
 context("Graph of Life (gol)")
 
-test_that("gol about", {
-              tmp_gol <- gol_about()
-              expect_true(inherits(tmp_gol, "gol"))
-              expect_equal(length(tmp_gol),  6L)
-          })
+test_that("gol about. If any of these tests break, it means that the doc for this function needs to be updated.", {
+    tmp_gol <- gol_about()
+    expect_true(inherits(tmp_gol, "gol"))
+    expect_equal(length(tmp_gol),  6L)
+    expect_true(all(names(tmp_gol) %in% c("graph_num_source_trees",
+                                          "graph_taxonomy_version",
+                                          "graph_num_tips",
+                                          "graph_root_name",
+                                          "graph_root_node_id",
+                                          "graph_root_ott_id")))
+})
 
 test_that("gol node info. If any of these tests break, it means that the doc for this function needs to be updated.", {
               expect_true(all(names(gol_node_info) %in%
