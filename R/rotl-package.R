596c930cc564ac1256f8a9e1265722ca7f1de110
Francois Michonneau <francois.michonneau@gmail.com>
2015-06-11 12:14:06 -0400

    add package documentation

--- R/rotl-package.R
+++ R/rotl-package.R
@@ -1,7 +1,45 @@
-#' rotl.
-#'
-#' @name rotl
-#' @docType package
-#' @import ape
-#' @import rncl
+##' An Interface to the Open Tree of Life API
+##'
+##' The Open Tree of Life is an NSF funded project that is generating
+##' an online, comprehensive phylogenetic tree for 1.8 million
+##' species. \code{rotl} provides an interface that allows you to
+##' query and retrive the parts of the tree of life that is of
+##' interest to you.
+##'
+##' \code{rotl} provides function to most of the end points the API
+##' provides. The documentation of the API is available at:
+##' \url{https://github.com/OpenTreeOfLife/opentree/wiki/Open-Tree-of-Life-APIs}
+##'
+##' @section Customizing API calls:
+##'
+##'     All functions that call API end points can take 2 arguments to
+##'     customize the API call.
+##'
+##'     \itemize{
+##'
+##'     \item{ \code{otl_v} } { This argument controls which version of
+##'     the API your call is using. The default value for this
+##'     argument is a call to the function \code{otl_version()} which
+##'     returns the current version of the Open Tree of Life APIs
+##'     (v2).}
+##'
+##'     \item{ \code{dev_url} } { This argument controls whether to use
+##'     the development version of the API. By default, \code{dev_url}
+##'     is set to \code{FALSE}, using \code{dev_url = TRUE} in your
+##'     function calls will use the development version.}
+##'
+##'     }
+##'
+##'
+##' @section Acknowledgments:
+##'
+##'     This package was started during the Open Tree of Life
+##'     \href{http://blog.opentreeoflife.org/2014/06/11/apply-for-tree-for-all-a-hackathon-to-access-opentree-resources/}{Hackathon}
+##'     organized by OpenTree, the NESCent Hackathon Interoperability
+##'     Phylogenetic group, and Arbor.
+##'
+##' @name rotl
+##' @docType package
+##' @import ape
+##' @import rncl
 NULL
