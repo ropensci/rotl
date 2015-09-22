654bb1cd32464c9eab278541cd2373c75b97cff4
Francois Michonneau <francois.michonneau@gmail.com>
2015-09-22 13:15:51 -0400

    separate methods for studies end point

--- R/studies.R
+++ R/studies.R
@@ -289,48 +289,6 @@ print.study_meta <- function(x, ...) {
     cat(paste0("  $nexml$", names(x$nexml)), sep="\n")
 }
 
-##' @export
-##' @rdname get_study_meta
-get_tree_ids <- function(sm) UseMethod("get_tree_ids")
-
-##' @export
-##' @rdname get_study_meta
-get_publication <- function(sm) UseMethod("get_publication")
-
-##' @export
-##' @rdname get_study_meta
-candidate_for_synth <- function(sm) UseMethod("candidate_for_synth")
-
-##' @export
-##' @rdname get_study_meta
-get_study_year <- function(sm) UseMethod("get_study_year")
-
-##' @export
-##' @rdname get_study_meta
-get_tree_ids.study_meta <- function(sm) {
-    unlist(sm[["nexml"]][["treesById"]][[sm[["nexml"]][["^ot:treesElementOrder"]][[1]]]][["^ot:treeElementOrder"]])
-}
-
-##' @export
-##' @rdname get_study_meta
-get_publication.study_meta <- function(sm) {
-    pub <- sm[["nexml"]][["^ot:studyPublicationReference"]]
-    attr(pub, "DOI") <- sm[["nexml"]][["^ot:studyPublication"]][["@href"]]
-    pub
-}
-
-##' @export
-##' @rdname get_study_meta
-candidate_for_synth.study_meta <- function(sm) {
-    unlist(sm[["nexml"]][["^ot:candidateTreeForSynthesis"]])
-}
-
-##' @export
-##' @rdname get_study_meta
-get_study_year.study_meta <- function(sm) {
-    sm[["nexml"]][["^ot:studyYear"]]
-}
-
 ##' Retrieve subtree from a specific tree in the Open Tree of Life data store
 ##'
 ##' @title Study Subtree
