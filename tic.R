c2b4072d7e7cc4748d6eed6980f88b5ad88e1c8f
François Michonneau <francois.michonneau@gmail.com>
2019-03-21 15:48:43 +0100

    use rotemplate for pkgdown site

--- tic.R
+++ tic.R
@@ -7,10 +7,14 @@ if (Sys.getenv("id_rsa") != "" &&
     ## - `ci()$is_tag()`: Only for tags, not for branches
     ## - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
     ## - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-    get_stage("before_deploy") %>%
-        add_step(step_setup_ssh())
+  get_stage("install") %>%
+    add_step(install_github("ropenscilabs/rotemplate"))
+
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
 
     get_stage("deploy") %>%
         add_step(step_build_pkgdown()) %>%
         add_step(step_push_deploy())
 }
+x
