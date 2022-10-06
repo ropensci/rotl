c462a8c953c0faf83e5bdce0cb8273c7b550a7eb
François Michonneau <francois.michonneau@gmail.com>
2022-10-06 16:55:44 +0200

    fix paths for vignette figures

--- vignettes/make-vignettes.R
+++ vignettes/make-vignettes.R
@@ -7,20 +7,9 @@ make_vignettes <- function() {
       knitr::knit(x, output = y)
     }
   )
-  ## clean figure directory if it exists
-  if (dir.exists("vignettes/figure")) {
-    unlink("vignettes/figure/", recursive = TRUE, force = TRUE)
-  }
-  ## recreate the directory
-  dir.create("vignettes/figure")
   ## move the figures
-  figures <- list.files("figure", pattern = "\\.png$", full.names = TRUE)
+  figures <- list.files(".", pattern = "fig-vignettes-.+\\.png$", full.names = TRUE)
   res_mv <- file.rename(figures, file.path("vignettes", figures))
   stopifnot(all(res_mv))
-  
-  ## remove base directory
-  unlink("figure", recursive = TRUE, force = TRUE)
-
-  ## returns TRUE is everything worked
-  invisible(!dir.exists("figure"))
+  TRUE
 }
